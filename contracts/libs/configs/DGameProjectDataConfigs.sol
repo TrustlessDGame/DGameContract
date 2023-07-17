9edf212251c682a748fdc1e9c7538a7b2ba515b8
Genesis Doge <doge666@generative.xyz>
2023-07-17 14:30:11 +0700

    - update

---
+++ contracts/libs/configs/DGameProjectDataConfigs.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+library  DGameProjectDataConfigs {
+    string public constant SCRIPT_PROVIDER = "SCRIPT_PROVIDER";
+    string public constant DECOMPRESS_LIB = "DECOMPRESS_LIB";
+}
