2ce0d6c14b5d8f5495bd99fe615017154d175230
genesis0000 <genesis0000@generative.xyz>
2023-08-25 17:19:01 +0700

    - update

--- migrations/services/bfs/3.2_store_with_path.ts
+++ migrations/services/bfs/3.2_store_with_path.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import {Bfs} from "./bfs";
+import pako from "pako";
 
 
 function sleep(ms: number) {
@@ -29,7 +30,17 @@ function getByteArray(filePath: string) {
         }
 
         const fileName = file.split("/")[file.split("/").length - 1];
-        const rawdata = getByteArray(file);
+        let rawdata = getByteArray(file);
+
+        console.log('rawdata', rawdata.length)
+        const compressedData = pako.gzip(rawdata);
+
+        // gzip
+        const base64CompressedData = Buffer.from(compressedData).toString('base64');
+        const dataURL = `data:@file/gzip;base64,${base64CompressedData}`;
+        console.log(dataURL)
+        rawdata = Buffer.from(dataURL)
+
         // partition rawdata into chunks
         const chunksize = 40_000;// max size
         let chunks = [];
@@ -41,7 +52,7 @@ function getByteArray(filePath: string) {
         console.log("Split to ", chunks.length);
         for (let i = 0; i < chunks.length; i++) {
             try {
-                const filePath = [path, fileName].join("/")
+                const filePath = [path, fileName, 'gzip'].join("-")
                 console.log('inscribe chunk', i, 'of file path', filePath, 'with', chunks[i].length, 'bytes');
                 const tx = await data.store(args[0], filePath, i, chunks[i], 29900000);// max gas limit
                 console.log("tx:", tx?.transactionHash, tx?.status);
@@ -50,7 +61,7 @@ function getByteArray(filePath: string) {
                 break;
             }
         }
-        console.log(['bfs:/', '42070', args[0], process.env.PUBLIC_KEY, path, fileName].join("/"))
+        console.log(['bfs:/', '42070', args[0], process.env.PUBLIC_KEY, path, fileName, 'gzip'].join("-"))
 
     } catch (e) {
         // Deal with the fact the chain failed
