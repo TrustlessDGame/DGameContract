4c3bf4147251fd8a7331253257883c771248385a
Vinkent420 <vinkent@newbitcoincity.com>
2024-05-06 14:59:12 +0700

    add script deploy

---
+++ migrations/deploy.js
@@ -0,0 +1,25 @@
+const {ethers } = require("hardhat");
+
+async function main() {
+
+    const [deployer] = await ethers.getSigners();
+
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const contractFactory = await ethers.getContractFactory("DelegateNode");
+    const contract = await contractFactory.deploy();
+
+    console.log("Contract deployed at:", contract.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
