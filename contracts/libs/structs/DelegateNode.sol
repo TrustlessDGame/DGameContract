b3e114380d778516c297f8d92499b5a3c890521d
kelvin2608 <2608@newbitcoincity.com>
2024-05-20 16:21:53 +0700

    Add event, pausable contract for DelegateNode

--- contracts/libs/structs/DelegateNode.sol
+++ contracts/libs/structs/DelegateNode.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-library DelegateNodeStruct {
+interface IDelegateNode {
     enum PoolStatus {
         INACTIVE,
         ACTIVE,
@@ -39,4 +39,28 @@ library DelegateNodeStruct {
         mapping(uint256 => uint256) caps; // claimed block => amount
         uint256[] blocks;   // list keys of caps
     }
+
+    // event
+    event CreatePool(PoolInfo poolInfo);
+    event Stake(address indexed user, uint256 amount, PoolInfo poolInfo);
+    event ActivePool(PoolInfo poolInfo);
+    event DeActivePool(PoolInfo poolInfo);
+    event UnStake(address user, uint256 amount, uint32 poolId, uint256 claimedBlock);
+    event ClaimUnStake(address user, uint256 amount, uint32 poolId, uint256 claimedBlock);
+    event ReStakeUnStakedPool(address user, uint256 unStakedAmount, uint32 oldPoolId, uint32 newPoolId);
+    event AdminWithdraw(address indexed to, uint256 amount, PoolInfo poolInfo);
+
+    event AdminChanged(address indexed oldAdmin, address indexed newAdmin);
+    event ModeratorChanged(address indexed oldModerator, address indexed newModerator);
+    event AmountToActiveupdated(address indexed caller, uint32 indexed poolId, uint256 oldAmount, uint256 newAmount);
+    event PoolNameUpdated(uint32 indexed poolId, string oldName, string newName);
+    event PoolImageUpdated(uint32 indexed poolId, string oldImage, string newImage);
+    event PoolFeePercentUpdated(uint32 indexed poolId, uint32 oldFeePercent, uint32 newFeePercent);
+    event MinerAddressUpdated(uint32 indexed poolId, address oldAddress, address newAddress);
+    event UserClaimReward(address indexed caller, uint32 indexed poolId, uint256 amount);
+    event MinerReceiveReward(address indexed miner, uint32 indexed poolId, uint256 amount);
+
+    // errors
+    error FailedTransfer();
+
 }
