8ae9df0b86227d96ec7f316972ec1e8a85f43d82
alex <alex@bvm.network>
2024-09-23 21:04:35 +0700

    add deploy zk script

---
+++ deploy/deployDelegateNode.js
@@ -0,0 +1,31 @@
+const { deployOrUpgrade } = require("./utils");
+
+const archiveConstructor = ["0x0Aeb9DddE49632d59CE86a7d95334073ECE73Fdd", "0x0Aeb9DddE49632d59CE86a7d95334073ECE73Fdd"];
+
+async function main(hre) {
+    console.log(`Deploying Delegate Node...`);
+    const networkConfig = hre.network.config;
+    try {
+        const constructorArguments = [...archiveConstructor, process.env.WALLET_PUBLIC_KEY];
+        const contract = await deployOrUpgrade(process.env.DELETEGATE_NODE_ADDRESS, 'DelegateNode', constructorArguments, networkConfig, true);
+        if (!contract) {
+            throw new Error(`Failed to deploy Delegate Node`);
+        }
+    } catch (e) {
+        console.log(e.message);
+        throw e;
+    }
+    console.log(`Finish deploy Delegate Node-------`);
+}
+
+module.exports = main;
+
+// This ensures the script will execute the `main` function when run directly via `npx hardhat run`
+if (require.main === module) {
+    require("hardhat").then((hre) => {
+        main(hre).catch((error) => {
+            console.error(error);
+            process.exitCode = 1;
+        });
+    });
+}
