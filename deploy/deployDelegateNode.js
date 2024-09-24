ee388e2172eaeb3d10fed16b43ed8932e02b84d1
alex <alex@bvm.network>
2024-09-24 10:47:39 +0700

    Update deploy script to accept erc20 address when init

--- deploy/deployDelegateNode.js
+++ deploy/deployDelegateNode.js
@@ -4,7 +4,11 @@ async function main(hre) {
     console.log(`Deploying Delegate Node...`);
     const networkConfig = hre.network.config;
     try {
-        const constructorArguments = [process.env.ADMIN_ADDRESS, process.env.POOL_ADMIN_ADDRESS, process.env.MODERATOR_ADDRESS];
+        const constructorArguments = [
+            process.env.ERC20_ADDRESS,
+            process.env.ADMIN_ADDRESS,
+            process.env.POOL_ADMIN_ADDRESS,
+            process.env.MODERATOR_ADDRESS];
         const contract = await deployOrUpgrade(process.env.DELETEGATE_NODE_ADDRESS, 'DelegateNode', constructorArguments, networkConfig, true);
         if (!contract) {
             throw new Error(`Failed to deploy Delegate Node`);
